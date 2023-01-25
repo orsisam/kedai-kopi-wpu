0ecd1ddedcd8f7f1588e2602a5ec100fa9811f1a
Isro Hidayatulloh <orsi.amax@gmail.com>
2023-01-25 21:07:20 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Learning Path - Frontend
+
+This the implementation of tutorial from Web Programming Unpas (WPU Channel) - [http://bit.ly/3HuXZYu](http://bit.ly/3HuXZYu)
---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hambburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar sidebar untuk menghilangkan navbar
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
